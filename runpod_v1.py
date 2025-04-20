6a6ddd15ce5f0bdb59a9273ff23c6fa9f04b0af9
hgabha <157254510+hgabha@users.noreply.github.com>
2025-04-20 14:34:22 +0530

    Update runpod_v1.py
    HIDream Full

--- runpod_v1.py
+++ runpod_v1.py
@@ -432,3 +432,29 @@ WAN21_KIJAI = [
         "directory": "clip_vision", "filename": ""
     },
 ]
+HIDREAM_FULL = [
+    {
+        "url": "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/resolve/main/split_files/diffusion_models/hidream_i1_full_fp16.safetensors?download=true",
+        "directory": "diffusion_models", "filename": ""
+    },
+    {
+        "url": "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/blob/main/split_files/vae/ae.safetensors?download=true",
+        "directory": "vae", "filename": ""
+    },
+    {
+        "url": "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/blob/main/split_files/text_encoders/clip_l_hidream.safetensors?download=true",
+        "directory": "text_encoders", "filename": ""
+    },
+    {
+        "url": "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/blob/main/split_files/text_encoders/clip_g_hidream.safetensors?download=true",
+        "directory": "text_encoders", "filename": ""
+    },
+    {
+        "url": "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/blob/main/split_files/text_encoders/t5xxl_fp8_e4m3fn_scaled.safetensors?download=true",
+        "directory": "text_encoders", "filename": ""
+    },
+    {
+        "url": "https://huggingface.co/Comfy-Org/HiDream-I1_ComfyUI/blob/main/split_files/text_encoders/llama_3.1_8b_instruct_fp8_scaled.safetensors?download=true",
+        "directory": "text_encoders", "filename": ""
+    }
+]
