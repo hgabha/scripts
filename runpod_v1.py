dec736251e3aae00e5fb36ea7da7a6be5a0336b0
hgabha <157254510+hgabha@users.noreply.github.com>
2025-07-29 06:43:01 +1000

    Update runpod_v1.py
    Wan 2.2 Added

--- runpod_v1.py
+++ runpod_v1.py
@@ -553,6 +553,46 @@ WAN21_MISC = [
         "directory": "clip_vision", "filename": ""
     },
 ]
+WAN22_MISC = [
+        {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors?download=true",
+        "directory": "text_encoders", "filename": ""
+    },
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan2.2_vae.safetensors?download=true",
+        "directory": "vae", "filename": ""
+    },
+]
+WAN22_TI2V_HN = [
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp16.safetensors?download=true",
+        "directory": "diffusion_models", "filename": ""
+    },
+]
+WAN22_TI2V_LN = [
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp16.safetensors?download=true",
+        "directory": "diffusion_models", "filename": ""
+    },
+]
+WAN22_TI2V_5B = [
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_ti2v_5B_fp16.safetensors?download=true",
+        "directory": "diffusion_models", "filename": ""
+    },
+]
+WAN22_T2VFP8_HN = [
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors?download=true",
+        "directory": "diffusion_models", "filename": ""
+    },
+]
+WAN22_T2VFP8_LN = [
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors",
+        "directory": "diffusion_models", "filename": ""
+    },
+]
 WAN21_KIJAI = [
     {
         "url": "https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan2_1-I2V-14B-480P_fp8_e4m3fn.safetensors?download=true",
