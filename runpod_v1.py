834eb8dced575d7abf1c9f482377137dc8670f2d
hgabha <157254510+hgabha@users.noreply.github.com>
2025-07-29 21:18:01 +1000

    Update runpod_v1.py

--- runpod_v1.py
+++ runpod_v1.py
@@ -563,13 +563,21 @@ WAN22_MISC = [
         "directory": "vae", "filename": ""
     },
 ]
-WAN22_TI2V_HN = [
+WAN22_TI2V_FP8 = [
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors?download=true",
+        "directory": "diffusion_models", "filename": ""
+    },
+    {
+        "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors?download=true",
+        "directory": "diffusion_models", "filename": ""
+    },
+]
+WAN22_TI2V = [
     {
         "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp16.safetensors?download=true",
         "directory": "diffusion_models", "filename": ""
     },
-]
-WAN22_TI2V_LN = [
     {
         "url": "https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp16.safetensors?download=true",
         "directory": "diffusion_models", "filename": ""
